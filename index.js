f5e0b584165db85ad7a84318be1c815c0d51b60a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-11-27 14:12:24 -0300

    docker

---
+++ index.js
@@ -0,0 +1,18 @@
+function countDuplicate(numbers) {
+  // Write your code here
+  let unique = new Set(numbers)
+  for (let i = 0; i < numbers.length; i++) {
+    
+  }
+}/* function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const base10 = parseInt(readLine().trim(), 10);
+
+  const result = changeAds(base10);
+
+  ws.write(result + '\n');
+
+  ws.end();
+} */    
+console.log(countDuplicate([1,1,2,3,2]))
